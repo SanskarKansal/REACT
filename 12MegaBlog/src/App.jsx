cefe87fcbe90ac1eb8bf4c2b4649991d74ae6d4b
Sanskar Kansal <sanskarkansal42@gmail.com>
2024-12-24 19:01:52 +0530

    auth work

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,15 +1,37 @@
-// import { useState } from 'react'
-
+import { useState,useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import './App.css'
+import authService from "./appwrite/auth"
+import {login, logout} from "./store/authSlice"
+import { Footer, Header } from './components'
 
 function App() {
-  // const [count, setCount] = useState(0)
-  console.log(import.meta.env.VITE_APPWRITE_URL)
-  return (
-    <>
-      <h1>project</h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true)
+  const dispatch =useDispatch()
+
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
+
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
